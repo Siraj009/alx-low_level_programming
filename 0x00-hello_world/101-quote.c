c5f42d89d88fcaeb04782c5252499484ec8af418
siraj009 <sirajaschalew@gmail.com>
2023-03-16 13:33:30 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+fwrite("and that piece of art is useful\" - Dora Kor", 43, 1, stderr);
+fwrite("par, 2015-10-19\n", 16, 1, stderr);
+return (1);
+}
