7930711d466323a5ed98c5c2d6eaa23fc9c7a9c1
siraj009 <sirajaschalew@gmail.com>
2023-03-16 13:07:25 -0700

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char sa[] = "\"Programming is like building a multilingual puzzle";/**string*/
+
+	puts(sa);
+	return (0);
+}
