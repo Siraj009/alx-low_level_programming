890e083deae7c029c3e1d8bb17c1dca8f7406b2c
siraj009 <sirajaschalew@gmail.com>
2023-03-16 13:16:20 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");/**string*/
+	printf("with proper grammar, but the outcome is a piece of art,/n");/**string*/
 
 		return (0);
 }
