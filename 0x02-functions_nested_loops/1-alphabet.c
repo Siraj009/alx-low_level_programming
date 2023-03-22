a574cab15a71947a241ce76f0ccebb0c468cdae4
siraj009 <sirajaschalew@gmail.com>
2023-03-22 13:32:05 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+	{
+		int i = 97;
+
+		while (i <= 122)
+		{
+			putchar(i);
+			i++;
+		}
+		putchar('\n');
+	}
